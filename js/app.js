8f0041fc8c98878cdbe88dd426cedd6a4f10b8d0
Nonolism <ruanvitormoreno@outlook.com>
2024-08-30 21:38:46 -0300

    Adicionar função pra adicionar produto ao carrinho

--- js/app.js
+++ js/app.js
@@ -4,11 +4,24 @@
 // quando clicar em limpar
 //     tirar itens da lista 
 //     zerar o total
+carrinho = document.getElementById('lista-produtos');
 
 function adicionar(){
-    produto = document.getElementById('produto').value;
-    valor = produto.replace(/\D/g, '');
-    quantidade = document.getElementById('quantidade').value;
+    var produto = document.getElementById('produto').value;
+    let partesProduto = produto.split(' - R$');
+
+    var nomeProduto = partesProduto[0];
+    valorProduto = partesProduto[1];
+
+    var quantidade = document.getElementById('quantidade').value;
+
+    if (quantidade > 0){
+        let item_carrinho = document.createElement('section');
+        item_carrinho.classList.add('carrinho__produtos__produto');
+        item_carrinho.innerHTML = `<span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${valorProduto}</span>`;
+        carrinho.appendChild(item_carrinho);
+    }
+
 }
 
 function limpar(){
