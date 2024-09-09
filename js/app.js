a2cdc7be1ef9a7b66a74dd9664eb6c5468c42f31
Nonolism <70848785+nonolism@users.noreply.github.com>
2024-09-09 00:11:16 -0300

    Comentar código para melhor entendimento

--- js/app.js
+++ js/app.js
@@ -14,25 +14,26 @@ function adicionar(){
         item_carrinho.classList.add('carrinho__produtos__produto');
         item_carrinho.innerHTML = `<span class="texto-azul">${parseInt(quantidade.value)}x</span> ${nomeProduto} <span class="texto-azul">R$${valorProduto}</span>`;
         carrinho.appendChild(item_carrinho);
-    }
-    else {
+    } else {
         alert('Quantidade Inválida, insira um número maior que 0');
-    }
-    quantidade.value = 0;
+    }//Condicional para adicionar itens ao carrinho, impedindo que sejam adicionados itens com quantidade igual ou menor a 0
+    
+    quantidade.value = 0; //Retornar quantidade ao valor padrão após input do usuário
 
     itemsCarrinho = carrinho.getElementsByClassName('carrinho__produtos__produto');
     for (let i = 0; i< itemsCarrinho.length; i++){
         let valorProdutoAtual = parseFloat(itemsCarrinho[i].querySelectorAll('.texto-azul')[1].innerHTML.match(/\d+/g));
         let quantidadeItemAtual = parseInt(itemsCarrinho[i].querySelectorAll('.texto-azul')[0].innerHTML.match(/\d+/g));
         valor = valor + (valorProdutoAtual * quantidadeItemAtual);
-    }
+    }//Loop para calculo do valor total do carrinho
+    
     valorTotal.innerText = `R$${valor}`;
 }
 
 function limpar(){
     while (carrinho.firstChild){
     carrinho.removeChild(carrinho.lastChild);
-    }
+    }//Loop para remover todos os itens do carrinho
     valorTotal.innerText = 'R$0';
     quantidade.value = 0;
 }
